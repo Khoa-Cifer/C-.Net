a128e8ff8b05aae5bbf22769018e932de8e13119
Khoa <ulquiorra10203@gmail.com>
2024-11-06 18:31:22 +0700

    update pe 2

---
+++ PRN212-PE-2/PE_PRN212_SU24_118801_TranDangKhoa/Euro.DAL/Repositories/FootballTeamRepository.cs
@@ -0,0 +1,20 @@
+﻿using Euro.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euro.DAL.Repositories
+{
+    public class FootballTeamRepository
+    {
+        private GermanyEuro2024DbContext _context;
+
+        public List<FootballTeam> GetAll()
+        {
+            _context = new();
+            return _context.FootballTeams.ToList();
+        }
+    }
+}
